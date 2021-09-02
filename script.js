e61e04337b01941cd44f21e770b5dd72e651f4ff
Diego Minelli <dodi@gft.com>
2021-09-02 11:37:14 -0300

    Criando a funcao de resetar o quiz

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ let correctAnswers = 0;
 
 showQuestion();
 
-// Fiunctions
+//Events
+document
+  .querySelector(".scoreArea button")
+  .addEventListener("click", resetEvent);
+
+// Functions
 function showQuestion() {
   if (questions[currentQuestion]) {
     let q = questions[currentQuestion];
@@ -68,4 +73,10 @@ function finishQuiz() {
   document.querySelector(".scoreArea").style.display = "block";
   document.querySelector(".questionArea").style.display = "none";
   document.querySelector(".progress--bar").style.width = "100%";
+}
+
+function resetEvent() {
+  correctAnswers = 0;
+  currentQuestion = 0;
+  showQuestion();
 }
